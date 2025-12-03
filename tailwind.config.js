8b750f608373a29b6a5157d8cbbeaa25b94bfc57
Tegast <tegast@ya.ru>
2025-12-03 09:34:45 +0500

    Включил переключение темы

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+  // Эта строчка включает ручное переключение темы:
+  darkMode: 'selector', 
+  
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
